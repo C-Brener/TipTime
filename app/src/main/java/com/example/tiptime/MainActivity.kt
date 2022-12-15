bd18e2358bc897ab54053cc6b36012753294359d
Caíque Brener <brenercaique0806@gmail.com>
2022-12-15 07:29:44 -0300

    Update UI

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -74,7 +76,10 @@ fun EditNumberField() {
         },
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentWidth(align = Alignment.CenterHorizontally)
+            .wrapContentWidth(align = Alignment.CenterHorizontally),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType =  KeyboardType.Number) ,
+        label = { Text(stringResource(id = R.string.cost_of_service)) }
     )
 }
 
