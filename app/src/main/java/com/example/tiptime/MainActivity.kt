be398a0619a494763fd8301aeca9b4dfb80ead9f
Caíque Brener <brenercaique0806@gmail.com>
2022-12-15 22:45:49 -0300

    State elevation and finish to development

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,11 +24,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +51,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeScreen() {
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount = amount)
+
     Column(modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
 
 //        O spacedBy serve para adicionar um espaçamento fixo entre os filhos dentro do Column
@@ -59,29 +66,38 @@ fun TipTimeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField()
+        EditNumberField(amountInput){
+            amountInput = it
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
 
     }
 }
 
 @Composable
-fun EditNumberField() {
-    var amountInput by remember {
-        mutableStateOf("")
-    }
+fun EditNumberField(value: String, onValueChange: (String) -> Unit) {
     TextField(
-        value = amountInput,
-        onValueChange = {
-            amountInput = it
-        },
+        value = value,
+        onValueChange = onValueChange,
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentWidth(align = Alignment.CenterHorizontally),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType =  KeyboardType.Number) ,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         label = { Text(stringResource(id = R.string.cost_of_service)) }
     )
 }
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0) =
+    NumberFormat.getCurrencyInstance().format((tipPercent / 100) * amount)
+
 
 @Preview(showSystemUi = true)
 @Composable
@@ -104,4 +120,13 @@ fun DefaultPreview() {
 //que no processo de recomposição o valor do estado não retorne ao valor inicial.
 
 //O remember utiliza-se do by para delegar o getter e setter da varíavel para o remember
+
+//Elevação de estado acontece sempre que é necessário compartilhar o estado com varias funções, ou criar um elemento sem estado
+//fixo para ser reutilizavél.
+// Quando a elevação de estado acontece, geralmente é introduzidos dois parâmentros numa função
+    //value:T -> O valor atual a ser mostrado
+    // onValueChange: (T) -> Unit, um lambda que é acionado quando o valor muda
+    // para que dentro da função que contra o estado possa ser alterado seu valor
+
+//Formatação posicional é quando utilizamos de forma dinâmica a string atualizando o valor e uma posição dentro do seu conteudo
 
